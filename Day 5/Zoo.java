849376a0235b6031a6395424fb7191a4d1599662
jalaluddin94 <jalaluddin.almursyidy@ikonsultan.com>
2022-07-19 20:18:10 +0700

    Day 5 - Latihan OOP

---
+++ Day 5/Zoo.java
@@ -0,0 +1,57 @@
+class Animal{
+    private String name;
+    private String color;
+
+    public Animal(String name, String color){
+        this.name = name;
+        this.color = color;
+    }
+
+    public Animal() {
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setColor(String color){
+        this.color = color;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getColor(){
+        return this.color;
+    }
+}
+
+public class Zoo{
+    public static void main(String[] args){
+        Animal animal = new Animal();
+
+        animal.setName("kucing");
+        animal.setColor("black");
+
+        System.out.println("Animal name: " + animal.getName());
+        System.out.println("Animal color: " + animal.getColor());
+    }
+}
+
+// class Buffalo extends Animal{
+//     private int weight;
+
+//     public Buffalo(){}
+
+//     public Buffalo(String name, String color, int weight){
+//         super(name, color);
+//         this.weight = weight;
+//     }
+
+//     public void showData(){
+//         System.out.println(name);
+//         System.out.println(color);
+//         System.out.println(this.weight);
+//     }
+// }
