9493e27c1dddeb750b6caa2453eea336f137ff93
jalaluddin94 <jalaluddin.almursyidy@ikonsultan.com>
2022-07-20 07:02:59 +0700

    Jawaban soal no.2 - Day 5 - Jalaluddin

---
+++ Day 5/Calculator.java
@@ -0,0 +1,190 @@
+import java.util.*;
+
+abstract class TemplateCalculator{
+    public abstract void tempMessage(String message);
+    public Integer selectedControl;
+
+    public void pagerBetisAtas(){
+        System.out.println("++++++++++++++++++++CALCULATOR++++++++++++++++++++");
+    }
+
+    public void pagerBetisBawah(){
+        System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++");
+    }
+
+    public void pagerBetisTengah(){
+        System.out.println("--------------------------------------------------");
+    }
+}
+
+class CalcControl extends TemplateCalculator{
+    @Override
+    public void tempMessage(String message){
+        System.out.println(message);
+    }
+
+    public Integer getSelectedControl(){
+        return this.selectedControl;
+    }
+
+    public void setSelectedControl(Integer sc){
+        this.selectedControl = sc;
+    }
+
+    public void interfaceControl(){
+        this.pagerBetisAtas();
+        this.tempMessage("1: Open calculator");
+        this.tempMessage("99: Exit");
+        this.pagerBetisTengah();
+        System.out.print("Pilihan anda: ");
+        
+        // return this.listenInputan();
+    }
+}
+
+class OperationControl extends TemplateCalculator{
+
+    @Override
+    public void tempMessage(String message){
+        System.out.println(message);
+    }
+
+    public Integer getSelectedOPControl(){
+        return this.selectedControl;
+    }
+
+    public void setSelectedOPControl(Integer sp){
+        this.selectedControl = sp;
+    }
+
+    public void interfaceControl(){
+        this.pagerBetisAtas();
+        this.tempMessage("Please enter calculation operation:");
+        this.tempMessage("1: ADD");
+        this.tempMessage("2: SUBSTRACT value");
+        this.tempMessage("3: MULTIPLY value");
+        this.tempMessage("4: DIVIDE value");
+        this.tempMessage("99: Exit");
+        this.pagerBetisTengah();
+        System.out.print("Pilihan anda: ");
+
+        // return this.listenInputan();
+    }
+}
+
+class ValueControl extends TemplateCalculator{
+    private Integer value1;
+    private Integer value2;
+    private Float result;
+
+    @Override
+    public void tempMessage(String message){
+        System.out.println(message);
+    }
+
+    public Integer getVal1(){
+        return this.value1;
+    }
+
+    public void setVal1(Integer v){
+        this.value1 = v;
+    }
+
+    public Integer getVal2(){
+        return this.value2;
+    }
+
+    public void setVal2(Integer v){
+        this.value2 = v;
+    }
+
+    public Float getVal3(){
+        return this.result;
+    }
+
+    public void setVal3(Float v){
+        this.result = v;
+    }
+
+    public void interfaceControlVal1(){
+        this.pagerBetisAtas();
+        this.tempMessage("Please 1st and 2nd value below:");
+        System.out.print("Value 1: ");
+    }
+
+    public void interfaceControlVal2(){
+        System.out.print("Value 2: ");
+    }
+
+    public Float operationMath(Integer selectedControl){
+        switch(selectedControl){
+            case 1: // ADD
+                Float addition = (float) (this.getVal1() + this.getVal2());
+                this.setVal3(addition);
+                break;
+            case 2: // SUBSTRACT
+                Float substraction = (float) (this.getVal1() - this.getVal2());
+                this.setVal3(substraction);
+                break;
+            case 3: // MULTIPLY
+                Float multi = (float) (this.getVal1() * this.getVal2());
+                this.setVal3(multi);
+                break;
+            case 4: // DIVISION
+                Float division = (float) (this.getVal1() / this.getVal2());
+                this.setVal3(division);
+                break;
+            default:
+                Float zero = (float) 0;
+                this.setVal3(zero);
+
+        }
+        return this.getVal3();
+    }
+}
+
+public class Calculator {
+    public static void main(String[] args){
+        CalcControl cc = new CalcControl();
+        cc.interfaceControl();
+
+        Scanner scannerControl = new Scanner(System.in);
+        Integer pilihanControl = scannerControl.nextInt();
+
+        cc.setSelectedControl(pilihanControl);
+        cc.pagerBetisBawah();
+
+        if(cc.getSelectedControl() == 1){
+            OperationControl oc = new OperationControl();
+            oc.interfaceControl();
+
+            pilihanControl = scannerControl.nextInt();
+
+            oc.setSelectedOPControl(pilihanControl);
+            oc.pagerBetisBawah();
+
+            if(oc.getSelectedOPControl() != 99){
+                ValueControl vc = new ValueControl();
+
+                vc.interfaceControlVal1();
+                Integer v1 = scannerControl.nextInt();
+                vc.setVal1(v1);
+                vc.interfaceControlVal2();
+                Integer v2 = scannerControl.nextInt();
+                vc.setVal2(v2);
+                vc.pagerBetisTengah();
+                
+                Float hasil = vc.operationMath(oc.getSelectedOPControl());
+                System.out.println("Hasil: " + hasil);
+            }else{
+                scannerControl.close();
+                return;
+            }
+
+            scannerControl.close();
+        }else{
+            scannerControl.close();
+            return;
+        }
+    }
+}
