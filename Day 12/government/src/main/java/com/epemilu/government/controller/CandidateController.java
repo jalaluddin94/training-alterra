9fbf3429c9f6032e4b064e29951e91814f149322
Jalaluddin <jalaluddin.almursyidy@ikonsultan.com>
2022-07-29 18:27:23 +0700

    Answer Task - Day 12 - Jalaluddin

--- Day 12/government/src/main/java/com/epemilu/government/controller/CandidateController.java
+++ Day 12/government/src/main/java/com/epemilu/government/controller/CandidateController.java
@@ -7,10 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -21,8 +18,18 @@ public class CandidateController {
 
     @SneakyThrows(Exception.class)
     @PostMapping(path = "/candidate", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Object> addCandidate(@RequestBody CandidateDto dto){
+    public ResponseEntity<Object> addCandidate(@RequestBody CandidateDto candidateData){
         log.info("api /api/v1/candidate/add is executed...");
-        return candidateService.add(dto);
+        return candidateService.add(candidateData);
+    }
+
+    @SneakyThrows(Exception.class)
+    @PutMapping(path = "/candidate/{idCandidate}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> updateCandidate(
+            @PathVariable Long IdCandidate,
+            @RequestBody CandidateDto candidateData
+    ){
+        log.info("api /api/v1/candidate/update is executed...");
+        return candidateService.update(IdCandidate, candidateData);
     }
 }
--- Day 12/government/src/main/java/com/epemilu/government/services/CandidateService.java
+++ Day 12/government/src/main/java/com/epemilu/government/services/CandidateService.java
@@ -1,8 +1,10 @@
 package com.epemilu.government.services;
 
 import com.epemilu.government.dto.CandidateDto;
+import com.epemilu.government.dto.ErrorResponse;
 import com.epemilu.government.models.Candidate;
 import com.epemilu.government.repository.CandidateRepository;
+import io.swagger.annotations.ApiOperation;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -10,8 +12,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.transaction.Transactional;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 @Service
 @Transactional(rollbackOn = Exception.class)
@@ -20,6 +26,7 @@ public class CandidateService {
     private CandidateRepository candidateRepository;
 
     @SneakyThrows(Exception.class)
+    @ApiOperation("Add new candidate")
     public ResponseEntity<Object> add(CandidateDto candidateReq){
 
         HttpHeaders headers = new HttpHeaders();
@@ -33,5 +40,26 @@ public class CandidateService {
         candidateRepository.save(theCandidate);
 
         return ResponseEntity.status(HttpStatus.CREATED).headers(headers).build();
+    }
+
+    @SneakyThrows(Exception.class)
+    @ApiOperation("Update candidate data")
+    public ResponseEntity<Object> update(Long idCandidate, CandidateDto candidateReq){
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        Candidate candidate = candidateRepository.findById(idCandidate).orElse(null);
+        if(Optional.ofNullable(candidate).isPresent()){
+            candidate.setCandidateName(candidateReq.getCandidateName());
+            candidate.setUsername(candidateReq.getUsername());
+            candidate.setAddress(candidateReq.getAddress());
+            candidateRepository.save(candidate);
+        }else{
+            ErrorResponse theError = new ErrorResponse("9999", "Data not found");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).headers(headers).body(theError);
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).headers(headers).body(candidateReq);
     }
 }
